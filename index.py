8451167665b816cd35cf9953c34b4ffcb6af404b
Niels Veen <hello@nielsveen.com>
2022-03-14 16:15:53 +0100

    tried different routes

--- index.py
+++ index.py
@@ -8,23 +8,26 @@ def home():
 
 @app.route('/webhook',methods=['GET','POST'])
 def webhook():
-    data = request.json
-    if data['page'] == "https://tickets.titanmaker.io/lesson/lesson-1/":
-        submit_id = data['submit_id']
-        email = data['email']
-        cro_address = data['cro_address']
-        ip_address = data['ip']
-        print('check with db')
-        db = upload_to_db(submit_id,email,cro_address,ip_address)
+    if request.method == 'POST':
+        data = request.json
+        if data['page'] == "https://tickets.titanmaker.io/lesson/lesson-1/":
+            submit_id = data['submit_id']
+            email = data['email']
+            cro_address = data['cro_address']
+            ip_address = data['ip']
+            print('check with db')
+            db = upload_to_db(submit_id,email,cro_address,ip_address)
 
-        if db == True:
-            # Code you want to run if the record does not exist
-            print('hello')
-        else:
-            # Code that runs if record already exists
-            print('nice')
-        return 'success', 200
-    else: return 'error', 404
+            if db == True:
+                # Code you want to run if the record does not exist
+                return 'Account is added!'
+            else:
+                # Code that runs if record already exists
+                return 'Account already existed'
+            return 'success', 200
+        else: return 'error', 404
+    else:
+        return 'Webhook page!'
 
 
 if __name__ == "__main__":
